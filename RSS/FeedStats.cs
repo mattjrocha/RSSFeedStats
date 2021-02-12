ce884670fa084cd5da62b2d3422e26568bcf6ebf
mrocha <mrocha@iadvantagesoftware.com>
2021-02-12 01:27:46 -0500

    Created a class that holds a site name, url, and inactivity string.  This class can be unit tested with NUnit as well as run by the UI. Its main purpose is to create a syndication based on an xml string created by the site url.  It uses that syndication to determine the newest feed on the site. The user can then get the amount of days since a site has published an article.

---
+++ RSS/FeedStats.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.ServiceModel.Syndication;
+using System.Xml;
+using NUnit.Framework;
+
+namespace RSSFeedStats.RSS
+{
+    /*
+     * FeedStats class takes a sitename and siteurl and returns the inactivity from that date.
+    */
+    [TestFixture]
+    public class FeedStats
+    {
+        public string SiteName;
+        public string SiteURL;
+        public string SiteInactivity;
+
+        /* Constructor Method
+         */
+        public FeedStats(string siteName, string siteURL)
+        {
+            SiteName = siteName;
+            SiteURL = siteURL;
+            SiteInactivity = SetInactivity();
+        }
+        /*
+         *For Testing Purposes only
+         */
+        [SetUp]
+        public void SetUp()
+        {
+            SiteName = "CBS Boston";
+            SiteURL = "https://boston.cbslocal.com/feed/";
+        }
+
+        /* SET INACTIVITY
+         * Uses get Inactivity to set the objects 
+         * days since last active feed.
+         */
+        [Test]
+        private string SetInactivity()
+        {
+            SyndicationFeed RSSFeed;
+            try
+            {
+                XmlReader xmlReader = XmlReader.Create(SiteURL);
+                try
+                {
+                    RSSFeed = SyndicationFeed.Load(xmlReader);
+                }
+                catch (Exception ex)
+                {
+                    string exception = ex.Message.ToString();
+                    return "N/A";//XML error
+                }
+                xmlReader.Close();
+                if (RSSFeed != null)
+                {
+                    return GetInactivity(RSSFeed).ToString();
+                }
+                else
+                {
+                    return "N/A";
+                }
+            }
+            catch(Exception ex)
+            {
+                string exception = ex.Message.ToString();
+                return "N/A";//error 404
+            }
+        }
+
+        /* GET INACTIVITY
+         * Subtracts todays date from the feeds publishing date
+         * Finds the newest published article for that feed
+         * Returns how many days from current date.
+         */
+        [Test]
+        private int GetInactivity(SyndicationFeed feed)
+        {
+            int lowInactivity = 999999;//certainly before internet
+            foreach (SyndicationItem item in feed.Items)
+            {
+                DateTime curDate = DateTime.UtcNow;
+                DateTime pubDate = item.PublishDate.UtcDateTime;
+                DateTime defDate = default(DateTime);//default date
+                if (!pubDate.Date.Equals(defDate.Date))//skip anything with default date.
+                {
+                    TimeSpan subDate = curDate.Subtract(pubDate.Date);
+                    bool success = int.TryParse(subDate.Days.ToString(), out int itemInactivity);
+                    if (itemInactivity < lowInactivity)//find newest active story
+                    {
+                        lowInactivity = itemInactivity;
+                    }
+                }
+
+            }
+            return lowInactivity;//days last feed
+        }
+
+    }
+}
